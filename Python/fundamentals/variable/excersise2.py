9f6d65bab7334dd6cf06275b7039245dbc0b89fe
koheitech <91676145+koheitech@users.noreply.github.com>
2021-11-27 22:15:06 +0900

    Delete Python/fundamentals/variable directory

--- Python/fundamentals/variable/excersise2.py
+++
@@ -1,5 +0,0 @@
-width = float(input("input width: "))
-length = float(input("input length: "))
-height = float(input("input height: "))
-volume = width * length * height
-print(f"the volume is {volume}.")
--- Python/fundamentals/variable/exersice3.py
+++
@@ -1,3 +0,0 @@
-cel = float(input('Please enter the temperature in celsius scale: '))
-fah = 32+cel*(9/5)
-print(f'It is going to be {fah} degree in fahrenheit scale.')
