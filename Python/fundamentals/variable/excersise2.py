25d3454c6c74777df34538a1e550b0396b8ba313
koheitech <91676145+koheitech@users.noreply.github.com>
2021-11-27 22:13:33 +0900

    Add files via upload

---
+++ Python/fundamentals/variable/excersise2.py
@@ -0,0 +1,5 @@
+width = float(input("input width: "))
+length = float(input("input length: "))
+height = float(input("input height: "))
+volume = width * length * height
+print(f"the volume is {volume}.")
